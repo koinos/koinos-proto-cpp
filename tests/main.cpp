8679273fa61f10cb3ffeec44048e18b45da0f052
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2023-12-07 13:12:46 -0800

    Refactor project structure for Pitchfork spec

---
+++ tests/main.cpp
@@ -0,0 +1,3 @@
+#define BOOST_TEST_MODULE koinos_proto_tests
+// OVERRIDE_BOOST_TEST_INCLUDED_WARNING
+#include <boost/test/included/unit_test.hpp>
---
+++ tools/ci/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [ "$RUN_TYPE" = "test" ]; then
+   cd $TRAVIS_BUILD_DIR/build/tests
+   exec ctest -j3 --output-on-failure
+fi
